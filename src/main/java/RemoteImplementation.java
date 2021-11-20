2ecdd7b8bb7804bfa4177341c6bb898778d82480
Aleksa <aleksas0308@gmail.com>
2021-11-20 03:12:04 +0100

    Updated Moving Folders.

--- src/main/java/RemoteImplementation.java
+++ src/main/java/RemoteImplementation.java
@@ -118,6 +118,75 @@ public class RemoteImplementation implements IODriver {
 
     @Override
     public void moveDirectory(String s, String s1) {
+        String srcFolderId = "";
+        String trgFolderId = "";
+        String pageToken = null;
+        do {
+            FileList result = null;
+            try {
+                result = driveService.files().list()
+                        .setSpaces("drive")
+                        .setFields("nextPageToken, files(id, name)")
+                        .setPageToken(pageToken)
+                        .execute();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            for (File file : result.getFiles()) {
+                if(file.getName().equals(s)) {
+                    srcFolderId = file.getId();
+                    System.out.printf("Found folder: %s (%s)\n",
+                            file.getName(), file.getId());
+                }
+            }
+            pageToken = result.getNextPageToken();
+        } while (pageToken != null);
+
+        pageToken = null;
+        do {
+            FileList result = null;
+            try {
+                result = driveService.files().list()
+                        .setSpaces("drive")
+                        .setFields("nextPageToken, files(id, name)")
+                        .setPageToken(pageToken)
+                        .execute();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            for (File file2 : result.getFiles()) {
+                if(file2.getName().equals(s1)) {
+                    trgFolderId = file2.getId();
+                    System.out.printf("Found folder: %s (%s)\n",
+                            file2.getName(), file2.getId());
+                }
+            }
+            pageToken = result.getNextPageToken();
+        } while (pageToken != null);
+
+        File file3 = null;
+        try {
+            file3 = driveService.files().get(srcFolderId)
+                    .setFields("parents")
+                    .execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        StringBuilder previousParents = new StringBuilder();
+        for (String parent : file3.getParents()) {
+            previousParents.append(parent);
+            previousParents.append(',');
+        }
+// Move the file to the new folder
+        try {
+            file3 = driveService.files().update(srcFolderId, null)
+                    .setAddParents(trgFolderId)
+                    .setRemoveParents(previousParents.toString())
+                    .setFields("id, parents")
+                    .execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
@@ -264,32 +333,35 @@ public class RemoteImplementation implements IODriver {
 
     @Override
     public void downloadFile(String s, String s1) {
-
-        FileList result = null;
-        try {
-            result = driveService.files().list()
-                    .setPageSize(10)
-                    .setFields("nextPageToken, files(id, name)")
-                    .execute();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        List<File> files = result.getFiles();
-        if (files == null || files.isEmpty()) {
-            System.out.println("No files found.");
-        } else {
-            for (File file : files) {
-                if (file.getName().equals(s)) {
-                    String fileId = file.getId();
+        String fileId = "";
+        String pageToken = null;
+        do {
+            FileList result = null;
+            try {
+                result = driveService.files().list()
+                        .setSpaces("drive")
+                        .setFields("nextPageToken, files(id, name)")
+                        .setPageToken(pageToken)
+                        .execute();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            for (File file : result.getFiles()) {
+                if(file.getName().equals(s)) {
+                    fileId = file.getId();
                     OutputStream outputStream = new ByteArrayOutputStream();
                     try {
-                        driveService.files().get(fileId).executeMediaAndDownloadTo(outputStream);
+                        driveService.files().get(fileId)
+                                .executeMediaAndDownloadTo(outputStream);
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
+                    System.out.printf("Found file: %s (%s)\n",
+                            file.getName(), file.getId());
                 }
             }
-        }
+            pageToken = result.getNextPageToken();
+        } while (pageToken != null);
 
     }
 
